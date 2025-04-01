9757af289eef232a468b2fa3f3d3d1c26baca8c4
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-01 12:48:41 +0300

    Add network info file

--- lib/core/network_info.dart
+++ lib/core/network_info.dart
@@ -1,11 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImpl(this.connectionChecker);
+
   @override
-  Future<bool> get isConnected async {
-    // Simulate network status (replace with actual implementation)
-    return Future.value(true); // Change to `false` for offline testing
-  }
+  Future<bool> get isConnected => connectionChecker.hasConnection;
 }
